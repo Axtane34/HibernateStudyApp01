2de341232975eaad84ce30fdd696fd64c2cd5620
Axtane <axtane3456@gmail.com>
2022-08-23 20:27:39 +0300

    fetchType hibernate practice (lazy, eager)

--- src/main/java/ru/axtane/App.java
+++ src/main/java/ru/axtane/App.java
@@ -1,5 +1,6 @@
 package ru.axtane;
 
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -25,9 +26,46 @@ public class App
             Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
 
+            Person person = session.get(Person.class, 2);
+            System.out.println("Получили человека");
 
+            //Получим связанные сущности (Lazy) - 2 запроса
+            //Hibernate.initialize(person.getItems()); //подгружаем связанные Lazy сущности
+
+            /*Item item = session.get(Item.class, 3);
+            System.out.println("Получили товар");
+
+            //Получим связанные сущности (Eager) - 1 запрос\left join
+            System.out.println(item.getOwner());*/
 
             session.getTransaction().commit();
+
+            System.out.println("Сессия завершилась (session.close)");
+
+            System.out.println("Вне сессии");
+
+            //открываем сессию и транзакцию еще раз, можно сделать в любом месте кода
+            session = sessionFactory.getCurrentSession();
+            session.beginTransaction();
+
+            System.out.println("Внутри второй транзакции");
+
+            //нужно делать только в первом случае, при hql запросе в этом нет нужды
+            person = (Person) session.merge(person);
+            //первый вариант, предпочтительный
+            //Hibernate.initialize(person.getItems());
+            //второй вариант, избыточный
+            List<Item> items = session.createSelectionQuery("select i from Item i where i.owner.id=:person_id", Item.class)
+                    .setParameter("person_id", person.getId()).getResultList();
+            System.out.println(items);
+            session.getTransaction().commit();
+
+            System.out.println("Вне второй сессии");
+            //это работает так как связанные товары были загружены
+            //System.out.println(person.getItems());
+
+            //после указания fetch = FetchType.EAGER даже в состоянии detach можно получить товары по геттеру объекта
+            //System.out.println(person.getItems());
         }
     }
 }
